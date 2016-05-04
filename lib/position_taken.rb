be4e03ab738fcbf32ed4919c73c14569328943d2
Nick Fulgoney <nickfull@gmail.com>
2016-05-04 16:03:41 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
-# code your #position_taken? method here!
+def position_taken? (board, position)
+ if board[0] == " " || board[0] == ""
+    false
+ elsif board[0] == nil
+    false
+ else true
+ end
+end
